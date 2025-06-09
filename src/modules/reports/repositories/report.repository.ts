774d7b616846f3ecc9ecd4c35dc8ae6a9df0cb78
Labib <labib@clodeo.com>
2025-06-09 11:21:55 +0700

    fix(report): get status returned invalid object

--- src/modules/reports/repositories/report.repository.ts
+++ src/modules/reports/repositories/report.repository.ts
@@ -5,13 +5,14 @@ import { ReportModel } from '@/db/models/report.model';
 
 @Injectable()
 export class ReportRepository implements IReportRepository {
-  findOne(id: string): Promise<ReportEntity | null> {
-    return ReportModel.findOne({ where: { id }, plain: true });
+  async findOne(id: string): Promise<ReportEntity | null> {
+    const record = await ReportModel.findOne({ where: { id } });
+    return record ? record.toJSON() : null;
   }
 
   async create(report: Partial<ReportEntity>): Promise<ReportEntity> {
     const insertedReport = await ReportModel.create(report);
-    return insertedReport.toJSON<ReportEntity>();
+    return insertedReport.toJSON();
   }
 
   async update(id: string, update: Partial<ReportEntity>): Promise<boolean> {
